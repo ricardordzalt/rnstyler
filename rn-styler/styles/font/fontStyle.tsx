99726a53f0fbe988b98c79ace05c0d3dde90ddaf
ricardo <ricardo.rdzalt@gmail.com>
2022-05-01 12:58:26 -0600

    fixed import of fontstyle

--- rn-styler/styles/font/fontStyle.tsx
+++ rn-styler/styles/font/fontStyle.tsx
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-export const fontSizeProperties = StyleSheet.create({
+export const fontStyleProperties = StyleSheet.create({
   'font-normal': {fontStyle: 'normal'},
   'font-italic': {fontStyle: 'italic'},
 });
--- rn-styler/styles/font/index.tsx
+++ rn-styler/styles/font/index.tsx
@@ -1,7 +1,9 @@
 import {fontSizeProperties} from './fontSize';
+import {fontStyleProperties} from './fontStyle';
 
 const fontStyles = {
   ...fontSizeProperties,
+  ...fontStyleProperties,
 };
 
 export {fontStyles};
