9aee251205f6b5d65048a138134cd43deaac80d4
ricardo <ricardo.rdzalt@gmail.com>
2022-05-01 12:56:04 -0600

    Added fontStyle

---
+++ rn-styler/styles/font/fontStyle.tsx
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+
+export const fontSizeProperties = StyleSheet.create({
+  'font-normal': {fontStyle: 'normal'},
+  'font-italic': {fontStyle: 'italic'},
+});
