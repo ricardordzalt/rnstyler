c9935a38ae4f206153528363687859b7791b7f26
ricardordzalt <ricardo.rdzalt@gmail.com>
2022-05-01 21:08:02 -0600

    Exported core components from react native

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import RNStyler, {ThemeProvider, View} from './rn-styler';
+import RNStyler, {View, Pressable, Text} from './rn-styler';
 
 const App = () => {
   return (
-    <RNStyler.View className="flex-1 justify-center items-center bg-#f00000">
-      <RNStyler.Pressable className="w-wp(50) bg-red-300 rounded-hp(4) py-hp(1) shadow">
-        <RNStyler.Text className="color-#00ff0b font-size-hp(3) text-center">
+    <View className="flex-1 justify-center items-center bg-#f00000">
+      <Pressable className="w-wp(50) bg-red-300 rounded-hp(4) py-hp(1) shadow">
+        <Text className="color-#00ff0b font-size-hp(3) text-center">
           Texto 1
-        </RNStyler.Text>
-      </RNStyler.Pressable>
-      <RNStyler.Text className="font-size-40 mt-hp(2) text-green-600">
+        </Text>
+      </Pressable>
+      <Text className="font-size-40 mt-hp(2) text-green-600">
         Texto 2
-      </RNStyler.Text>
-    </RNStyler.View>
+      </Text>
+    </View>
   );
 };
 
--- rn-styler/index.tsx
+++ rn-styler/index.tsx
@@ -3,4 +3,48 @@ export {ThemeProvider} from './context/Context';
 export {useColors} from './context/Context';
 export {useStyler} from './styler';
 
+const ActivityIndicator = styler.ActivityIndicator;
+const Button = styler.Button;
+const FlatList = styler.FlatList;
+const Image = styler.Image;
+const ImageBackground = styler.ImageBackground;
+const KeyboardAvoidingView = styler.KeyboardAvoidingView;
+const Modal = styler.Modal;
+const Pressable = styler.Pressable;
+const RefreshControl = styler.RefreshControl;
+const ScrollView = styler.ScrollView;
+const SectionList = styler.SectionList;
+const StatusBar = styler.StatusBar;
+const Switch = styler.Switch;
+const Text = styler.Text;
+const TextInput = styler.TextInput;
+const TouchableHighlight = styler.TouchableHighlight;
+const TouchableOpacity = styler.TouchableOpacity;
+const TouchableWithoutFeedback = styler.TouchableWithoutFeedback;
+const View = styler.View;
+const VirtualizedList = styler.VirtualizedList;
+
+export {
+  ActivityIndicator,
+  Button,
+  FlatList,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Modal,
+  Pressable,
+  RefreshControl,
+  ScrollView,
+  SectionList,
+  StatusBar,
+  Switch,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+  VirtualizedList,
+}
+
 export default styler;
